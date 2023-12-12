78a9231e280294618fd45143939480f20fe528c0
camilod529 <camilod529@gmail.com>
2023-12-12 12:40:36 -0500

    usando Link de next, instalado iconos de Octicons

--- 01-first-steps/components/navbar/Navbar.tsx
+++ 01-first-steps/components/navbar/Navbar.tsx
@@ -1,18 +1,36 @@
+import Link from "next/link";
+
+import { HomeIcon } from "@primer/octicons-react";
+
+const navItems = [
+    {
+        name: "About",
+        url: "/about",
+    },
+    {
+        name: "Pricing",
+        url: "/pricing",
+    },
+    {
+        name: "Contact",
+        url: "/contact",
+    },
+];
+
 export const Navbar = () => {
     return (
         <>
             <nav className="flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded">
-                <span>Home</span>
+                <Link href={"/"} className="flex items-center">
+                    <HomeIcon className="mr-2" />
+                    <span>Home</span>
+                </Link>
                 <div className="flex flex-1"></div>
-                <a className="mr-2" href="/about">
-                    about
-                </a>
-                <a className="mr-2" href="/pricing">
-                    pricing
-                </a>
-                <a className="mr-2" href="/contact">
-                    Contact
-                </a>
+                {navItems.map((item) => (
+                    <Link key={item.url} href={item.url} className="mr-2">
+                        {item.name}
+                    </Link>
+                ))}
             </nav>
         </>
     );
