17546bd688cc4065b21c4b2ef4879ff6acb39d32
camilod529 <camilod529@gmail.com>
2023-12-12 11:40:22 -0500

    creadas paginas about, contact, pricing, editado metatag de root layout

---
+++ 01-first-steps/app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function AboutPage() {
+    return (
+        <>
+            <span className="text-7xl">about page</span>
+        </>
+    );
+}
--- 01-first-steps/app/page.tsx
+++ 01-first-steps/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <span className="text-5xl">Hola mundo!</span>
+            {new Date().toLocaleTimeString()}
         </main>
     );
 }
