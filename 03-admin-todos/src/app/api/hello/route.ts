5261de0bc054ed6ad6258a07cc0bd505efdb1cf8
camilod529 <camilod529@gmail.com>
2023-12-13 16:20:22 -0500

    creacion inicial proyecto admin todos, iniciando con creacion de RESTFul API endpoints

---
+++ 03-admin-todos/src/app/api/hello/route.ts
@@ -0,0 +1,8 @@
+import { NextResponse, NextRequest } from "next/server";
+
+export async function GET(request: Request) {
+    return NextResponse.json({ message: "Hello World GET" });
+}
+export async function POST(request: Request) {
+    return NextResponse.json({ message: "Hello World post" });
+}
