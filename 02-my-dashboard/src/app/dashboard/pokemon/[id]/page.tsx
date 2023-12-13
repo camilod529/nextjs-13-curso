95b15c818488fb9bba57cf8c5e222f88143122d1
camilod529 <camilod529@gmail.com>
2023-12-13 09:44:32 -0500

    metadata dinamica

--- 02-my-dashboard/src/app/dashboard/pokemon/[id]/page.tsx
+++ 02-my-dashboard/src/app/dashboard/pokemon/[id]/page.tsx
@@ -1,12 +1,25 @@
 import { Pokemon } from "@/pokemons";
+import { Metadata } from "next";
 
 interface Props {
     params: { id: string };
 }
 
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+    const { id, name } = await getPokemon(params.id);
+
+    return {
+        title: `# ${id} - ${name}`,
+        description: `Pagina del pokemon ${name}`,
+    };
+}
+
 const getPokemon = async (id: string): Promise<Pokemon> => {
     const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {
         cache: "force-cache",
+        // next: {
+        //     revalidate: 60 * 60 * 30 * 6,
+        // }
     }).then((res) => res.json());
     console.log(`se cargo ${pokemon.name}`);
     return pokemon;
@@ -17,7 +30,7 @@ export default async function PokemonPage({ params }: Props) {
     return (
         <div>
             <h1>Pokemon {params.id}</h1>
-            <div>{JSON.stringify(pokemon)}</div>
+            <div>{pokemon.name}</div>
         </div>
     );
 }
