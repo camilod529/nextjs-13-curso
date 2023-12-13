42a4a8ede7e23efa3e0b13922c182bbebfc81ea3
camilod529 <camilod529@gmail.com>
2023-12-13 08:28:15 -0500

    fetch en pagina asincrona

--- 02-my-dashboard/src/app/dashboard/pokemons/page.tsx
+++ 02-my-dashboard/src/app/dashboard/pokemons/page.tsx
@@ -1,7 +1,12 @@
-export default function PokemonsPage() {
-    return (
-        <div>
-            <h1>Hello Page Pokemons</h1>
-        </div>
-    );
+const getPokemons = async (limit = 20, offset = 0) => {
+    const data = await fetch(
+        `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
+    ).then((res) => res.json());
+
+    return data;
+};
+
+export default async function PokemonsPage() {
+    const pokemons = await getPokemons();
+    return <div>{JSON.stringify(pokemons)}</div>;
 }
