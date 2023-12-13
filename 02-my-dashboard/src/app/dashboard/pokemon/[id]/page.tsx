59d3b50945da546e96f56a4c74fdfa6050998526
camilod529 <camilod529@gmail.com>
2023-12-13 09:39:28 -0500

    fetch de pokemon individual para cada pagina

--- 02-my-dashboard/src/app/dashboard/pokemon/[id]/page.tsx
+++ 02-my-dashboard/src/app/dashboard/pokemon/[id]/page.tsx
@@ -1,12 +1,23 @@
+import { Pokemon } from "@/pokemons";
+
 interface Props {
     params: { id: string };
 }
 
-export default function PokemonPage({ params }: Props) {
-    console.log(params);
+const getPokemon = async (id: string): Promise<Pokemon> => {
+    const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {
+        cache: "force-cache",
+    }).then((res) => res.json());
+    console.log(`se cargo ${pokemon.name}`);
+    return pokemon;
+};
+
+export default async function PokemonPage({ params }: Props) {
+    const pokemon = await getPokemon(params.id);
     return (
         <div>
             <h1>Pokemon {params.id}</h1>
+            <div>{JSON.stringify(pokemon)}</div>
         </div>
     );
 }
