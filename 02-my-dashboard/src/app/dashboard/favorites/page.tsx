2220502c851959ae0dbb6304dd145516d2a31a2b
camilod529 <camilod529@gmail.com>
2023-12-13 11:29:49 -0500

    simple widget para dashboard main page

---
+++ 02-my-dashboard/src/app/dashboard/favorites/page.tsx
@@ -0,0 +1,17 @@
+import { PokemonGrid, PokemonsResponse, SimplePokemon } from "@/pokemons";
+
+export const metadata = {
+    title: "Favoritos",
+    description: "Pokemones favoritos",
+};
+
+export default async function FavoritePokemonsPage() {
+    return (
+        <div className="flex flex-col">
+            <span className="text-5xl my-2">
+                Listado de Pokemons favoritos <small className="text-blue-500">Global estate</small>
+            </span>
+            <PokemonGrid pokemons={[]} />
+        </div>
+    );
+}
