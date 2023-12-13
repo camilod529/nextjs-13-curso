2fe9b8245d0eb2aa0bcb67841030612315c878ad
camilod529 <camilod529@gmail.com>
2023-12-13 11:53:47 -0500

    usando acciones del estado global en redux

--- 02-my-dashboard/src/shopping-cart/components/CartCounter.tsx
+++ 02-my-dashboard/src/shopping-cart/components/CartCounter.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useAppDispatch, useAppSelector } from "@/store";
+import { decrement, increment } from "@/store/counter/counterSlice";
 import { useState } from "react";
 
 interface Props {
@@ -7,21 +9,22 @@ interface Props {
 }
 
 export const CartCounter = ({ value = 0 }: Props) => {
-    const [counter, setCounter] = useState(value);
+    const count = useAppSelector((state) => state.counter.count);
+    const dispatch = useAppDispatch();
 
     return (
         <>
-            <span className="text-9xl">{counter}</span>
+            <span className="text-9xl">{count}</span>
             <div className="flex">
                 <button
                     className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2"
-                    onClick={() => setCounter(counter + 1)}
+                    onClick={() => dispatch(increment())}
                 >
                     +1
                 </button>
                 <button
                     className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2"
-                    onClick={() => setCounter(counter - 1)}
+                    onClick={() => dispatch(decrement())}
                 >
                     -1
                 </button>
--- 02-my-dashboard/src/store/counter/counterSlice.ts
+++ 02-my-dashboard/src/store/counter/counterSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
 interface CounterState {
     count: number;
@@ -16,11 +16,16 @@ const counterSlice = createSlice({
             state.count += 1;
         },
         decrement: (state) => {
+            if (state.count === 0) return;
             state.count -= 1;
+        },
+        resetCount: (state, action: PayloadAction<number>) => {
+            if (action.payload < 0) action.payload = 0;
+            state.count = action.payload;
         },
     },
 });
 
-export const { decrement, increment } = counterSlice.actions;
+export const { decrement, increment, resetCount } = counterSlice.actions;
 
 export default counterSlice.reducer;
