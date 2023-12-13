606c0caec88e033eef7a308d053c86430c9337fc
camilod529 <camilod529@gmail.com>
2023-12-13 10:06:14 -0500

    not-found global y pagina de not-found para fallo en pokemons

--- 02-my-dashboard/src/app/dashboard/pokemon/[id]/page.tsx
+++ 02-my-dashboard/src/app/dashboard/pokemon/[id]/page.tsx
@@ -1,29 +1,41 @@
 import { Pokemon } from "@/pokemons";
 import { Metadata } from "next";
 import Image from "next/image";
+import { notFound } from "next/navigation";
 
 interface Props {
     params: { id: string };
 }
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
-    const { id, name } = await getPokemon(params.id);
+    try {
+        const { id, name } = await getPokemon(params.id);
 
-    return {
-        title: `# ${id} - ${name}`,
-        description: `Pagina del pokemon ${name}`,
-    };
+        return {
+            title: `# ${id} - ${name}`,
+            description: `Pagina del pokemon ${name}`,
+        };
+    } catch (error) {
+        return {
+            title: `# ${params.id} - No encontrado`,
+            description: `Pagina del pokemon ${params.id}`,
+        };
+    }
 }
 
 const getPokemon = async (id: string): Promise<Pokemon> => {
-    const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {
-        cache: "force-cache",
-        // next: {
-        //     revalidate: 60 * 60 * 30 * 6,
-        // }
-    }).then((res) => res.json());
-    console.log(`se cargo ${pokemon.name}`);
-    return pokemon;
+    try {
+        const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {
+            cache: "force-cache",
+            // next: {
+            //     revalidate: 60 * 60 * 30 * 6,
+            // }
+        }).then((res) => res.json());
+        console.log(`se cargo ${pokemon.name}`);
+        return pokemon;
+    } catch (error) {
+        notFound();
+    }
 };
 
 export default async function PokemonPage({ params }: Props) {
