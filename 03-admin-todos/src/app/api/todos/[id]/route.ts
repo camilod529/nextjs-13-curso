e7e344171a75548a6bfdf9c7ddf8afe6f75fd318
camilod529 <camilod529@gmail.com>
2023-12-13 19:39:44 -0500

    PUT para editar todo

--- 03-admin-todos/src/app/api/todos/[id]/route.ts
+++ 03-admin-todos/src/app/api/todos/[id]/route.ts
@@ -1,5 +1,6 @@
 import prisma from "@/lib/prisma";
 import { NextResponse, NextRequest } from "next/server";
+import * as yup from "yup";
 
 interface Segments {
     params: {
@@ -18,4 +19,32 @@ export async function GET(request: Request, { params }: Segments) {
 
     if (!todo) return NextResponse.json({ error: `Todo with id ${id} not found` }, { status: 404 });
     return NextResponse.json(todo);
+}
+
+const putSchema = yup.object({
+    description: yup.string().required(),
+    complete: yup.boolean().optional().default(false),
+});
+
+export async function PUT(request: Request, { params }: Segments) {
+    const { id } = params;
+
+    const todo = await prisma.todo.findUnique({
+        where: {
+            id,
+        },
+    });
+
+    if (!todo) return NextResponse.json({ error: `Todo with id ${id} not found` }, { status: 404 });
+    try {
+        const { description, complete } = await putSchema.validate(await request.json());
+        const updatedTodo = await prisma.todo.update({
+            where: { id },
+            data: { description, complete },
+        });
+
+        return NextResponse.json(updatedTodo);
+    } catch (error) {
+        return NextResponse.json({ error: error }, { status: 400 });
+    }
 }
